296c05c0eadbb48d9e69758c0819073bf78c6030
Will Szumski <will@stackhpc.com>
2019-10-07 16:08:51 +0100

    Add some tests and make them green

--- molecule/tests/test_default.py
+++ molecule/tests/test_default.py
@@ -6,9 +6,30 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_directories(host):
+    dirs = [
+        "/etc/ipmi-exporter"
+    ]
+    for dir in dirs:
+        d = host.file(dir)
+        assert d.is_directory
+        assert d.exists
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+def test_files(host):
+    files = [
+        "/etc/ipmi-exporter/ipmi.yml",
+    ]
+    for file in files:
+        f = host.file(file)
+        assert f.exists
+        assert f.is_file
+
+
+def test_socket(host):
+    sockets = [
+        "tcp://127.0.0.1:9290"
+    ]
+    for socket in sockets:
+        s = host.socket(socket)
+        assert s.is_listening
